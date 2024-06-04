26f38e619c3ae678a09f8a792fa51438d051ef9e
Aagam995 <aagamjain080@gmail.com>
2024-06-04 16:54:37 +0530

    welcome 001

---
+++ story_content/triggers.js
@@ -0,0 +1,105 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5cIaMswsLce":
+        Script1();
+        break;
+      case "5jt99uiOzXk":
+        Script2();
+        break;
+      case "68XaFrBJHHF":
+        Script3();
+        break;
+      case "6Q5PidqvLj5":
+        Script4();
+        break;
+      case "6qp0oWS8iRv":
+        Script5();
+        break;
+      case "6eOfRxy1QvU":
+        Script6();
+        break;
+      case "5ttLJmOQI7S":
+        Script7();
+        break;
+      case "5g1VX1mFspm":
+        Script8();
+        break;
+      case "66HViYVr24b":
+        Script9();
+        break;
+      case "6iy45ILexsG":
+        Script10();
+        break;
+      case "5bKBoau5XJL":
+        Script11();
+        break;
+      case "6YUwusytkMu":
+        Script12();
+        break;
+      case "6lEceaGkg9B":
+        Script13();
+        break;
+      case "6WdQSM4Xz6H":
+        Script14();
+        break;
+      case "5YJ1c7XXfB0":
+        Script15();
+        break;
+      case "5zlkecc9TIf":
+        Script16();
+        break;
+      case "5sNyjyXHOPZ":
+        Script17();
+        break;
+      case "6pDxodW6rO1":
+        Script18();
+        break;
+      case "6kVEkZ0XNgy":
+        Script19();
+        break;
+      case "5vBxZx5cCjt":
+        Script20();
+        break;
+      case "5xgrQs1RtxO":
+        Script21();
+        break;
+      case "6pvRcTT8LQn":
+        Script22();
+        break;
+      case "6krAwIxUUMu":
+        Script23();
+        break;
+      case "6ZdwUP3pjJ5":
+        Script24();
+        break;
+      case "5ZY2O0s76Ae":
+        Script25();
+        break;
+      case "5cH7EPM1jXi":
+        Script26();
+        break;
+      case "6lOrb3AD4Kg":
+        Script27();
+        break;
+      case "6QzLwaMAhve":
+        Script28();
+        break;
+      case "6UhJOoM2kRN":
+        Script29();
+        break;
+      case "6S3H1I35KnR":
+        Script30();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
